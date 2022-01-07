580b260707ee02e6d1d3fff4b21c3c5b7d33111a
Student 249860 <lemihdesign@gmail.com>
2022-01-07 18:34:49 +0100

    Project site

--- js/script.js
+++ js/script.js
@@ -1,10 +1,32 @@
 window.addEventListener('scroll', function() {
-    console.log('dziala')
     const scrollFromTop = window.scrollY;
-    console.log(scrollFromTop)
     if(scrollFromTop > 50) {
         document.querySelector('header').classList.add('active');
     } else if(scrollFromTop ==0 ) {
         document.querySelector('header').classList.remove('active');
     }
+})
+
+document.querySelector('.wolontariusz').addEventListener('mouseover', function() {
+    document.querySelector('.wolontariuszImage').classList.add('showImage');
+    document.querySelector('.wolontariusz').classList.add('projectAreaLabel--active');
+    document.querySelector('.wolontariuszDesc').classList.add('projectAreaLabel--active');
+})
+
+document.querySelector('.wolontariusz').addEventListener('mouseleave', function() {
+    document.querySelector('.wolontariuszImage').classList.remove('showImage');
+    document.querySelector('.wolontariusz').classList.remove('projectAreaLabel--active');
+    document.querySelector('.wolontariuszDesc').classList.remove('projectAreaLabel--active');
+})
+
+document.querySelector('.portfolio').addEventListener('mouseover', function() {
+    document.querySelector('.portfolioImage').classList.add('showImage');
+    document.querySelector('.portfolio').classList.add('projectAreaLabel--active');
+    document.querySelector('.portfolioDesc').classList.add('projectAreaLabel--active');
+})
+
+document.querySelector('.portfolio').addEventListener('mouseleave', function() {
+    document.querySelector('.portfolioImage').classList.remove('showImage');
+    document.querySelector('.portfolio').classList.remove('projectAreaLabel--active');
+    document.querySelector('.portfolioDesc').classList.remove('projectAreaLabel--active');
 })
