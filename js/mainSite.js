a59186ba541762b4c89f83598c37bc4f4b608e39
Student 249860 <lemihdesign@gmail.com>
2022-01-13 01:17:32 +0100

    Update for landing page

--- js/mainSite.js
+++ js/mainSite.js
@@ -1,32 +1,13 @@
-window.addEventListener('scroll', function() {
-    const scrollFromTop = window.scrollY;
-    if(scrollFromTop > 50) {
-        document.querySelector('header').classList.add('active');
-    } else if(scrollFromTop ==0 ) {
-        document.querySelector('header').classList.remove('active');
+$(document).on('scroll', function() {
+    const scrollValue = $(this).scrollTop();
+    const $mainSection = $('.main-section');
+    const mainSectionFromTop = $mainSection.offset().top;
+    const mainSectionHeight = $mainSection.outerHeight();
+    
+    if(scrollValue > mainSectionFromTop + mainSectionHeight/2 - $(window).height()) {
+        $mainSection.addClass('activeTypography');
+    } 
+    if(scroll > mainSectionFromTop + mainSectionHeight - $(window).height()) {
+        $mainSection.removeClass('activeTypography')
     }
-})
-
-document.querySelector('.wolontariusz').addEventListener('mouseover', function() {
-    document.querySelector('.wolontariuszImage').classList.add('showImage');
-    document.querySelector('.wolontariusz').classList.add('projectAreaLabel--active');
-    document.querySelector('.wolontariuszDesc').classList.add('projectAreaLabel--active');
-})
-
-document.querySelector('.wolontariusz').addEventListener('mouseleave', function() {
-    document.querySelector('.wolontariuszImage').classList.remove('showImage');
-    document.querySelector('.wolontariusz').classList.remove('projectAreaLabel--active');
-    document.querySelector('.wolontariuszDesc').classList.remove('projectAreaLabel--active');
-})
-
-document.querySelector('.portfolio').addEventListener('mouseover', function() {
-    document.querySelector('.portfolioImage').classList.add('showImage');
-    document.querySelector('.portfolio').classList.add('projectAreaLabel--active');
-    document.querySelector('.portfolioDesc').classList.add('projectAreaLabel--active');
-})
-
-document.querySelector('.portfolio').addEventListener('mouseleave', function() {
-    document.querySelector('.portfolioImage').classList.remove('showImage');
-    document.querySelector('.portfolio').classList.remove('projectAreaLabel--active');
-    document.querySelector('.portfolioDesc').classList.remove('projectAreaLabel--active');
-})
+  })
